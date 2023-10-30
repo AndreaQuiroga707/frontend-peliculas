02c286468884b4e52876a042aa0b122dec5692a1
andrea <andrea.quiroga@ucb.edu.bo>
2023-10-30 00:59:32 -0400

    login con guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+//para bloquear rutas
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   {path: '**', redirectTo: 'login', pathMatch: 'full'}
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'frontend-peliculas';
+  isAuthenticated: boolean;
+
+  constructor(@Inject(DOCUMENT) private document: Document,
+              private authService: AuthService) {
+    this.isAuthenticated = false;
+  }
+
+  public ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe((success: boolean) => {
+      this.isAuthenticated = success;
+    });
+  }
 }
+  
